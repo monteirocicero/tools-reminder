1e074e515148de8e43d25bfc952b7acccd32725c
Cicero Monteiro <cicerolmonteiro@gmail.com>
2020-10-11 20:03:27 -0300

    The API Doc

---
+++ src/main/kotlin/com/orecic/toolsreminder/ToolsReminderApplication.kt
@@ -0,0 +1,11 @@
+package com.orecic.toolsreminder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToolsReminderApplication
+
+fun main(args: Array<String>) {
+	runApplication<ToolsReminderApplication>(*args)
+}
---
+++ src/test/kotlin/com/orecic/toolsreminder/ToolsReminderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.orecic.toolsreminder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToolsReminderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
